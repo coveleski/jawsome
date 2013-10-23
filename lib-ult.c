9ed761318ee3d0e1920d7991aaf3c787e66e0765
David Vriezen <browzilla@gmail.com>
2013-10-23 18:09:25 -0500

    no message

---
+++ lib-ult.c
@@ -0,0 +1,21 @@
+#include <ucontext.h>
+
+
+
+void system_init(int max_number_of_klt){
+
+}
+
+int uthread_create(void func(), int pri_num){
+
+
+
+}
+
+int uthread_yield(int pri_num){
+
+}
+
+void uthread_exit(){
+	
+}
